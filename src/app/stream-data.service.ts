445bc70c10683cd0e47066d379fea648bc9be12d
loic.haye@ipsos.com <loic.haye@ipsos.com>
2018-10-10 16:46:23 +0200

    generate fake stream-data

--- src/app/stream-data.service.ts
+++ src/app/stream-data.service.ts
@@ -8,6 +8,7 @@ import { StreamDataModel } from './stream-data/stream-data-model';
 // import { DataConverterService } from './data-converter.service';
 
 // const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' }) };
+const streamDataModelToGenerateCount: number = 357;
 
 @Injectable({
   providedIn: 'root'
@@ -22,11 +23,45 @@ export class StreamDataService {
     { id: 3, streamProjectId: 1, rawData: 'rawData3', exportData: '', areDataExported: false, isStreamValid: false, contentMessage: 'data not valid', clientDatabaseName: 'clientDatabaseName', exportDate: null, insertDate: new Date(2018, 1, 1, 10, 55) }
   ];
 
-  constructor() { }
+  generateStreamDataModels(streamDataModelToGenerateCount: number): StreamDataModel[] {
+    let generatedStreamDataModels: StreamDataModel[] = [];
+    let insertOrExportDate: Date = new Date(2018, 0, 1);
+    for (let counter = 1; counter <= streamDataModelToGenerateCount; counter++) {
+
+      //0 ou 1
+      let randomNumberOne = Math.round(Math.random());
+      let randomNumberTwo = Math.round(Math.random());
+      let secondsToAdd = Math.round(Math.random() * 300000);
+
+      let isValid = (randomNumberOne == 1);
+      let areDataExported = (isValid && randomNumberTwo == 1);
+      insertOrExportDate = new Date(insertOrExportDate.getTime() + secondsToAdd * 1000);
+
+      let streamDataModel: StreamDataModel = {
+        id: counter,
+        streamProjectId: 1,
+        rawData: 'rawData' + counter,
+        exportData: 'exportData' + counter,
+        areDataExported: areDataExported,
+        isStreamValid: isValid,
+        contentMessage: isValid ? '' : 'error ' + counter,
+        clientDatabaseName: 'clientDatabaseName',
+        exportDate: isValid ? insertOrExportDate : null,
+        insertDate: insertOrExportDate
+      };
+      generatedStreamDataModels.unshift(streamDataModel);
+    }
+    return generatedStreamDataModels;
+  }
+
+  constructor() {
+    this.streamDataModels = this.generateStreamDataModels(streamDataModelToGenerateCount);
+  }
   // constructor(private http: HttpClient, private dataConverterService: DataConverterService) { }
 
   getStreamDataViewModel(inferiorInsertDate: Date, superiorInsertDate: Date): Observable<StreamDataViewModel> {
     let filteredStreamDataModels = this.streamDataModels;
+
     if (inferiorInsertDate) {
       filteredStreamDataModels = filteredStreamDataModels.filter(streamDataModel => streamDataModel.insertDate >= inferiorInsertDate);
     }
